0cd704a2771bb345c038ab514c4be31b56c96f31
Kenrick Guie <nyhunter77@yahoo.com>
2017-10-09 13:27:11 -0400

    small changes to make us heroku ready

--- server/index.js
+++ server/index.js
@@ -5,5 +5,7 @@ app.get('/', (req, res) => {
 	res.send({ hi: 'there' });
 });
 
-console.log('App listening on port 5000');
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+console.log(`App listening on port ${PORT}`);
+app.listen(PORT);
